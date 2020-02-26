480fcc9a3b18d8d0eae6a39cf4aa389263aeb44a
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-02-26 12:20:25 -0300

    CRUD de Categorias

--- routes/web.php
+++ routes/web.php
@@ -125,6 +125,7 @@ Route::group(['middleware' => ['auth']], function () {
 
         Route::resource('stores', 'StoreController');
         Route::resource('products', 'ProductController');
+        Route::resource('categories', 'CategoryController');
 
     });
 
