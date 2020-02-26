ec63718236042965550368e146a17a73c80c4a3a
Thifany Nicastro <thifanynicastro@hotmail.com>
2020-02-26 11:37:30 -0300

    Validação

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,21 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success my-3">Criar Loja</a>
+@if(!$store)
+<a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
+@else
 
 <table class='table table-striped'>
     <thead>
         <tr>
             <th>#</th>
             <th>Loja</th>
+            <th>Total de produtos</th>
             <th>Ações</th>
         </tr>
     </thead>
     <tbody>
-        @foreach($stores as $store)
         <tr>
             <td>{{$store->id}}</td>
             <td>{{$store->name}}</td>
+            <td>{{$store->products->count()}}</td>
             <td>
                 <div class="btn-group">
                     <a href="{{route('admin.stores.edit', ['store' => $store->id])}}"
@@ -28,9 +31,7 @@
                 </div>
             </td>
         </tr>
-        @endforeach
     </tbody>
 </table>
-
-{{ $stores->links() }}
+@endif
 @endsection
